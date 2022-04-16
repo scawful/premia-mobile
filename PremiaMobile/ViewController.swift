5e9ec5fdd1cbf2090e894dfd7d18e3a76100ac0c
Justin Scofield <justin.scofield@outlook.com>
2022-04-16 02:03:53 -0500

    Initial Commit

---
+++ PremiaMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PremiaMobile
+//
+//  Created by Justin Scofield on 4/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
